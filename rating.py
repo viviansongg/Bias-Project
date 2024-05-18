10ec51975062fca0dda5982e6ac1ded93de4bf34
Brianna <132308375+briannale935@users.noreply.github.com>
2024-05-18 17:10:49 -0400

    change logic to get rid of countryand category parameters

--- rating.py
+++ rating.py
@@ -1,5 +1,5 @@
 import news_api as NewsAPI
-from search import user_category, urls
+from search import urls
 from bs4 import BeautifulSoup
 import requests
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -10,6 +10,9 @@ neg_keyword= []
 analyzer = SentimentIntensityAnalyzer()
 
 def determine_bias(index):
+   if len(urls)==0: 
+       return ("No search results")
+   
    while index > len(urls):
        try:
            index= int(input("Index out of range. Enter a new article index: "))
@@ -40,109 +43,4 @@ def determine_bias(index):
    else:
         print("The article has a neutral sentiment.")
 
-
-#    if user_category=='business':
-#        pos_word_count=0
-#        neg_word_count=0
-#        pos_keyword=['growth', 'prosperity', 'promising', 'breakthrough', 'upsurge', 'record-breaking', 'boasted']
-#        neg_keyword=['stagnation', 'suffer', 'collapse', 'turmoil', 'warning', 'allegedly', 'supposedly']
-#        for word in text:
-#            if word in pos_keyword:
-#                pos_word_count+= 1
-#        for word in text:
-#            if word in neg_keyword:
-#                neg_word_count += 1
-#    if user_category=='entertainment':
-#        pos_word_count=0
-#        neg_word_count=0
-#        pos_keyword=['acclaimed', 'hit', 'thrilling', 'stunning', 'unforgettable', 'outstanding', 'iconic']
-#        neg_keyword=['boring', 'flawed', 'embarrassing', 'underwhelming', 'unimpressive', 'weak', 'worthless']
-#        for word in text:
-#            if word in pos_keyword:
-#                pos_word_count+= 1
-#        for word in text:
-#            if word in neg_keyword:
-#                neg_word_count += 1
-#    if user_category=='general':
-#        pos_word_count=0
-#        neg_word_count=0
-#        pos_keyword=['accomplish', 'celebrate', 'promising', 'exceptional', 'positive', 'promising', 'success']
-#        neg_keyword=['alarming', 'controversial', 'decline', 'disastrous', 'failure', 'risky', 'unfortunate']
-#        for word in text:
-#            if word in pos_keyword:
-#                pos_word_count+= 1
-#        for word in text:
-#            if word in neg_keyword:
-#                neg_word_count += 1
-#    if user_category=='health':
-#        pos_word_count=0
-#        neg_word_count=0
-#        pos_keyword=['advancement', 'cure', 'enhance', 'innovate', 'breakthrough', 'preventative', 'progress', 'resilient']
-#        neg_keyword=['adverse', 'crisis', 'suffer', 'epidemic', 'deteriorating', 'harmful', 'infectious']
-#        for word in text:
-#            if word in pos_keyword:
-#                pos_word_count+= 1
-#        for word in text:
-#            if word in neg_keyword:
-#                neg_word_count += 1
-#    if user_category=='science':
-#        pos_word_count=0
-#        neg_word_count=0
-#        pos_keyword=['breakthrough', 'revolutionary', 'advancements', 'cutting-edge', 'effective', 'unprecedented', 'transformative']
-#        neg_keyword=['flawed', 'ineffective', 'misleading', 'hazardous', 'outdated', 'unreliable', 'skeptical']
-#        for word in text:
-#            if word in pos_keyword:
-#                pos_word_count+= 1
-#        for word in text:
-#            if word in neg_keyword:
-#                neg_word_count += 1
-#    if user_category=='sports':
-#        pos_word_count=0
-#        neg_word_count=0
-#        pos_keyword=['growth', 'prosperity', 'promising', 'breakthrough', 'upsurge', 'record-breaking', 'boasted', 'promise']
-#        neg_keyword=['stagnation', 'suffer', 'collapse', 'turmoil', 'underwhelming', 'allegedly', 'supposedly']
-#        for word in text:
-#            if word in pos_keyword:
-#                pos_word_count+= 1
-#        for word in text:
-#            if word in neg_keyword:
-#                neg_word_count += 1
-#    if user_category=='technology':
-#        pos_word_count=0
-#        neg_word_count=0
-#        pos_keyword=['admirable', 'champion', 'stellar', 'triumphant', 'unstoppable', 'record-breaking', 'outstanding']
-#        neg_keyword=['disappointing', 'inferior', 'mediocre', 'outmatched', 'poor', 'setback', 'slump']
-#        for word in text:
-#            if word in pos_keyword:
-#                pos_word_count+= 1
-#        for word in text:
-#            if word in neg_keyword:
-#                neg_word_count += 1
-  
-#    # Ratio= pos word/ neg word
-#    # multiply by 5 to scale on a scale of 1-5
-#    # higher keyword_bias score = more bias towards positive content
-#    # lower keyword_bias score = more bias towards negative content
-#    if neg_word_count==0:
-#        print ("Positive Bias. No negative bias keywords found")
-  
-#    if pos_word_count==0:
-#        print ("Negative Bias. No positive bias keywords found")
-
-
-#    if pos_word_count>0 and neg_word_count>0:
-#        bias_score= (pos_word_count//neg_word_count)*5
-#        if bias_score >0 and bias_score<= 0.2:
-#            keyword_bias=1
-#        elif bias_score>0.2 and bias_score<=0.4:
-#            keyword_bias=2
-#        elif bias_score >0.4 and bias_score<=0.6:
-#            keyword_bias=3
-#        elif bias_score>0.6 and bias_score <=0.8:
-#            keyword_bias=4
-#        elif bias_score <0.8 and bias_score <=1:
-#            keyword_bias=5
-#        print(keyword_bias)
-#        return keyword_bias
-  
-determine_bias(4)
+determine_bias(2)
--- search.py
+++ search.py
@@ -2,9 +2,7 @@ import news_api as NewsAPI
 
 
 user_search = input("Enter a search: ")
-user_category = input("Enter a category: ")
-user_country= input("Enter a country: ")
-data = NewsAPI.fetch_news(user_search, user_category, user_country)
+data = NewsAPI.fetch_news(user_search)
 urls=NewsAPI.get_search_results(data)
 print(data)
 print(urls)
